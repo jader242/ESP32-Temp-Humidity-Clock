126558855f3d9505456e397fdfa67c24b771dd0c
jader242 <calebcretella@gmail.com>
2026-02-01 20:03:29 -0700

    Refactor screen refresh logic

--- README.md
+++ README.md
@@ -8,8 +8,16 @@ Just a simple clock that displays temperature and humidity, syncs ntp, and also 
 - M5Stack Unit OLED Screen
 
 **Future goals:**
-- reduce screen flicker
+- ✔  reduce screen flicker - No more 15 second delay and full screen refresh each loop. Now it checks for differences in the time, temp, and humidity and refreshes each component accordingly
 - implement multiple screens and a way to switch between them (ie m5stack dual button unit or scroll unit)
 - add a buzzer for an alarm clock
+- change auto brightness logic to not hammer the i2c bus each loop
 
 ![ESP32 Temp/Humidity Clock Display](/assets/display.jpeg)
+
+**How to Build (if you have all the same hardware and want to try it)**
+- Git clone repo
+- Add your WiFi SSID and Password to src/main.cpp (the lines are commented)
+- Change timezone for NTP if necessary (this line is also commented)
+- Have PlatformIO VS Code extension or CLI
+- 'pio run -t upload'
--- src/main.cpp
+++ src/main.cpp
@@ -11,12 +11,13 @@
 
 M5UnitOLED oled(27, 26, 400000); // use pins 26 and 27 for i2c sda/scl, 400kHz (mandatory to put this)
 
-const char* ssid = "YOUR_SSID_HERE";
-const char* password = "YOUR_PASSWORD_HERE";
+const char* ssid = "YOUR_WIFI_SSID"; // change this to your wifi ssid
+const char* password = "YOUR_WIFI_PASSWORD"; // change this to your wifi password
 WiFiUDP ntpUDP;
 
 NTPClient timeClient(ntpUDP, "pool.ntp.org", -25200, 60000); //change second arg for your timezone
 DHT dht(DHTPIN, DHTTYPE);
+int firstRun = 0;
 
 void setup() {
   Serial.begin(115200);
@@ -43,10 +44,9 @@ void setup() {
 
 void loop() {
   timeClient.update();
-  oled.fillScreen(0);
+  static int lastMinutes = -1;
   int hours = timeClient.getHours();
   int minutes = timeClient.getMinutes();
-  int seconds = timeClient.getSeconds();
   char timeStr[12];
   if (hours == 12) {
     sprintf(timeStr, "%02d:%02d PM", hours, minutes);
@@ -59,14 +59,17 @@ void loop() {
       sprintf(timeStr, "%02d:%02d PM", (hours - 12), minutes);
     }
   }
+  static float lastTemperatureF = NAN;
+  static float lastHumidity = NAN;
   float humidity = dht.readHumidity();
   float temperatureF = dht.readTemperature(true); //true for F
+  float correctedTemp = temperatureF - (temperatureF * 0.05); // my sensor reads a bit high, YMMV
   oled.setTextSize(2);
   int time_yoffset = oled.fontHeight();
   char tempStr[20];
   char humidityStr[20];
   if (!isnan(humidity) && !isnan(temperatureF)) { // check if humidity and temp are numbers
-    sprintf(tempStr, "%.1f F", temperatureF);  // one decimal place with 'F'
+    sprintf(tempStr, "%.1f F", correctedTemp);  // one decimal place with 'F', subtract 5% for accuracy
     sprintf(humidityStr, "%.0f%%", humidity); // no decimal place with % sign
   }else {
     sprintf(tempStr, "Temp Error");
@@ -75,15 +78,56 @@ void loop() {
   int timeX = (oled.width() - oled.textWidth(timeStr)) / 2;
   int tempX = (oled.width() - oled.textWidth(tempStr)) / 2;
   int humX = (oled.width() - oled.textWidth(humidityStr)) / 2;
-  if (hours >= 6 && hours < 21) {
+  if (hours >= 6 && hours < 21) { // need to change this in the future so its not writing to i2c bus every loop
     oled.setBrightness(128);
   }else {
     oled.setBrightness(64);
   }
-  oled.drawString(timeStr, timeX, 5);
-  oled.drawString(tempStr, tempX, (time_yoffset + 10));
-  oled.drawString(humidityStr, humX, ((time_yoffset + 10) + oled.fontHeight() + 5));
-  oled.display();
-  Serial.println("Running...");
-  delay(15000);
+  if (firstRun == 0) {
+    oled.drawString(timeStr, timeX, 5);
+    oled.drawString(tempStr, tempX, (time_yoffset + 10));
+    oled.drawString(humidityStr, humX, ((time_yoffset + 10) + oled.fontHeight() + 5));
+    oled.display();
+    firstRun++;
+    lastTemperatureF = temperatureF;
+    lastHumidity = humidity;
+    Serial.println("First Run Complete"); // idk why tf this prints so many times lmao
+  }
+  if (minutes != lastMinutes) {
+    int newHours = timeClient.getHours();
+    int newMinutes = timeClient.getMinutes();
+    lastMinutes = newMinutes;
+    char newTimeStr[12];
+    if (newHours == 12) {
+      sprintf(newTimeStr, "%02d:%02d PM", newHours, newMinutes);
+    }else if (newHours == 0) {
+      sprintf(newTimeStr, "%02d:%02d AM", (newHours + 12), newMinutes);
+    }else {
+      if (newHours < 12) {
+        sprintf(newTimeStr, "%02d:%02d AM", newHours, newMinutes);
+      }else {
+        sprintf(newTimeStr, "%02d:%02d PM", (newHours - 12), newMinutes);
+      }
+    }
+    oled.fillRect(0, 5, oled.width(), oled.fontHeight());
+    oled.drawString(newTimeStr, timeX, 5);
+    oled.display();
+    Serial.println("Updated Time");
+  }
+  if (!isnan(humidity) && !isnan(temperatureF)) {
+    if (fabs(correctedTemp - lastTemperatureF) > 0.5) { // only update when temp fluctuates more than 0.5 degrees to avoid over reading from the dht
+      lastTemperatureF = correctedTemp;
+      oled.fillRect(0, (time_yoffset + 10), oled.width(), oled.fontHeight());
+      oled.drawString(tempStr, tempX, (time_yoffset + 10));
+      oled.display();
+      Serial.println("Updated Temp");
+    }
+    if (fabs(humidity - lastHumidity) > 1.0) { // same thing here but if humidity fluctuates more than 1%
+      lastHumidity = humidity;
+      oled.fillRect(0, ((time_yoffset + 10) + (oled.fontHeight() + 5)), oled.width(), oled.fontHeight());
+      oled.drawString(humidityStr, humX, ((time_yoffset + 10) + oled.fontHeight() + 5));
+      oled.display();
+      Serial.println("Updated Humidity");
+    }
+  }
  }
